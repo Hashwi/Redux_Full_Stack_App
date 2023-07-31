a0332cd4b0fe77860fcacfaa866c86a0376877b7
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-07-31 10:53:17 +0200

    handling errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import {useSelector } from 'react-redux/es/hooks/useSelector';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
 
-  const showCart = useSelector(state => state.ui.cartIsVisible)
+  useEffect(() => {
+    fetch("https://react-http-379a1-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
-     { showCart && <Cart />}
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
